20fe63fceb298bb4a0bb44c7ed50e5c475eb1faa
sungjoon-park <sungjoon_park@waku-2.com>
2020-07-26 22:07:24 +0900

    Add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,17 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import type { RootState } from './store'
-import { handleInitialData } from './actions/shared'
-import Characters from './components/Characters'
-import Episodes from './components/Episodes'
-import Quotes from './components/Quotes'
-// import Deaths from './components/Deaths'
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage'
 
 const App: React.FC = () => {
-  const dispatch = useDispatch()
-  const isLoading = useSelector((state: RootState) => state.isLoading)
-  const { characters, episodes, quotes } = useSelector(
-    (state: RootState) => state.allFetchedData,
-  )
-
-  useEffect(() => {
-    dispatch(handleInitialData())
-  }, [dispatch])
-
   return (
-    <div style={{ padding: '2em 0 5em' }}>
-      <h1 data-testid="headingOne">Breaking Bad App</h1>
-      {(() => {
-        if (isLoading)
-          return <h2 data-testid="headingNowLoading">Now loading...</h2>
-        return (
-          <>
-            <Characters characters={characters} />
-            <Episodes episodes={episodes} />
-            <Quotes quotes={quotes} />
-            {/* <Deaths /> */}
-          </>
-        )
-      })()}
-    </div>
+    <Router>
+      <div style={{ padding: '2em 0 5em' }}>
+        <h1 data-testid="headingOne">Breaking Bad App</h1>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+        </Switch>
+      </div>
+    </Router>
   )
 }
 
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,39 @@
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import type { RootState } from '../store'
+import { handleInitialData } from '../actions/shared'
+import Characters from '../components/Characters'
+import Episodes from '../components/Episodes'
+import Quotes from '../components/Quotes'
+// import Deaths from './components/Deaths'
+
+const Homepage: React.FC = () => {
+  const dispatch = useDispatch()
+  const isLoading = useSelector((state: RootState) => state.isLoading)
+  const { characters, episodes, quotes } = useSelector(
+    (state: RootState) => state.allFetchedData,
+  )
+
+  useEffect(() => {
+    dispatch(handleInitialData())
+  }, [dispatch])
+
+  return (
+    <>
+      {(() => {
+        if (isLoading)
+          return <h2 data-testid="headingNowLoading">Now loading...</h2>
+        return (
+          <>
+            <Characters characters={characters} />
+            <Episodes episodes={episodes} />
+            <Quotes quotes={quotes} />
+            {/* <Deaths /> */}
+          </>
+        )
+      })()}
+    </>
+  )
+}
+
+export default Homepage
